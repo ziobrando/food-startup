92f6095196c69ba1ff734a2320b12296cc00a76a
Alberto Brandolini <alberto.brandolini@gmail.com>
2021-06-24 09:25:12 +0200

    Initial commit

---
+++ src/main/java/it/avanscoperta/foodstartup/FoodStartupApplication.java
@@ -0,0 +1,13 @@
+package it.avanscoperta.foodstartup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodStartupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodStartupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/avanscoperta/foodstartup/FoodStartupApplicationTests.java
@@ -0,0 +1,13 @@
+package it.avanscoperta.foodstartup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodStartupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
